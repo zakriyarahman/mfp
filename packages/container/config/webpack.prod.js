a2b93fa5c59da07045aa1f8ad323bebf258c485b
Zakaria Sharief <zakriyarahman@gmail.com>
2024-03-05 20:59:10 -0600

    Remove a comma for testing purposes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
             remotes: {
                 marketing: `marketing@${domain}/marketing/remoteEntry.js`
             },
-            shared: packageJson.dependencies,
+            shared: packageJson.dependencies
         }),
     ]
 };
