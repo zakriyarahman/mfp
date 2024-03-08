e99e9eb60845e16456670b55d6ef7d8e1f993e36
Zakaria Sharief <zakriyarahman@gmail.com>
2024-03-07 23:47:03 -0600

    Fixed styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 import { BrowserRouter } from 'react-router-dom';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
