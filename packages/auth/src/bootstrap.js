a8d3796efed2d27689af55ea0c66dd5585512b72
Zakaria Sharief <zakriyarahman@gmail.com>
2024-03-17 00:00:15 -0500

    Added auth subapp

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath],
+    });
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname}) {
+            if (history.location.pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_auth-dev-root');
+
+    if (devRoot) {
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+export { mount };
